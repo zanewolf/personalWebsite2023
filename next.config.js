52287450485c8e047b78453faf2b3d12c5688fca
Zane Wolf <rzanewolf@gmail.com>
2023-11-01 14:41:59 -0600

    updating next.config

--- next.config.js
+++ next.config.js
@@ -1,11 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	images: {
-		domains: ['images.ctfassets.net','downloads.ctfassets.net'],
+		remotePatterns: [
+			{
+				hostname: 'images.ctfassets.net',
+			},
+			{
+				hostname: 'downloads.ctfassets.net',
+			},
+		],//['images.ctfassets.net','downloads.ctfassets.net'],
 		deviceSizes: [600, 640, 750, 828, 1080, 1200, 1920, 2048, 3840],
-	},
-	experimental: {
-		serverActions: true,
 	}
 }
 
