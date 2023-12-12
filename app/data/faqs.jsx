2910fbc90d790a366aab817a18225192995788c8
Zane Wolf <rzanewolf@gmail.com>
2023-12-12 13:29:41 -0700

    updating faqs

--- app/data/faqs.jsx
+++ app/data/faqs.jsx
@@ -9,20 +9,20 @@ export const faqs=[
 		question: "If I could have a superpower, what would it be?",
 		answer:"I have two answers, the practical answer and the instinctual, heart's desire answer. My instinctual answer to this question is 'Shapeshifter'. Given my affinity for animals, it's really no surprise. The practical answer, the one I'd probably choose because the world is the way it is, is 'the ability to refill things'. Out of money? Refill your wallet. Out of gas? Refill your tank. Annoyed by someone? Refill their bladder. The ultimate power for a capitalist society."
 	},
-	{
-		category:"personal",
-		question: "What's something I consider unique about myself?",
-		answer:"In academia, I consider myself a 'jack of all trades' scientist, but I feel this label extends to almost every part of my life. Take my education for example: public school, private school, homeschool, community college, public university, and private university. Where I've lived: suburbs outside of Atlanta, inner city Atlanta, middle of nowhere Georgia, whatever you classify Somerville as (it's in the top 20 densest cities in the US but feels like a small town). I've also played a LOT of sports growing up. I'm not a specialist and never will be, but because of my diverse experiences and despite being a classic introverted wallflower, I can almost always find something with which to connect with others, or provide varies perspectives. Diversity is the spice of life."
-	},
+	// {
+	// 	category:"personal",
+	// 	question: "What's something I consider unique about myself?",
+	// 	answer:"In academia, I consider myself a 'jack of all trades' scientist, but I feel this label extends to almost every part of my life. Take my education for example: public school, private school, homeschool, community college, public university, and private university. Where I've lived: suburbs outside of Atlanta, inner city Atlanta, middle of nowhere Georgia, whatever you classify Somerville as (it's in the top 20 densest cities in the US but feels like a small town). I've also played a LOT of sports growing up. I'm not a specialist and never will be, but because of my diverse experiences and despite being a classic introverted wallflower, I can almost always find something with which to connect with others, or provide varies perspectives. Diversity is the spice of life."
+	// },
 	{
 		category:"professional",
 		question: "What is something I want to learn or be better at?",
-		answer:"I'm very analytically-minded, but I'm not very artistic or graphically creative. I rely heavily on inspiration. If I can see something, I can usually reverse-engineer how it was made or works. From there, I tweak and tinker to fit my (or a client's) needs. I believe strongly in improving upon the wheel, not reinventing it, so being a mimic usually works in my favor. That being said, I do want to be a stronger original designer, and that just means more practice."
+		answer:"I'm very analytically-minded, but I'm not very artistic. I recently heard this referred to as 'good at drawing in straight lines,' and I think that's a perfect summary. I'm good at reverse-engineering and I believe strongly in improving  the wheel, not reinventing it. I can usually find something close to what I had in mind and then tinker and modify from there, but I do want to be a stronger original artist."
 	},
 	{
 		category:"professional",
 		question: "Why data visualization?",
-		answer:"It combines everything I enjoyed about research and science: exploring data, finding the story, teaching and sharing what I love with others, the diversity of topics you interact with (unlike a phd topic, which is far less variable), and the constant learning and acquisition of skills needed to complete the project. I'm happiest when I'm learning something new or sharing something I just learned with others - and that's the entire point of data visualization. Why wouldn't I want to pursue it as a career?"
+		answer:"It combines everything I enjoyed about research and science: exploring data, finding the story, teaching and sharing what I love with others, the diversity of topics you interact with (unlike a phd topic, which is far less variable), and the constant learning and acquisition of skills needed to complete the project. I'm happiest when I'm learning something new or sharing something I just learned with others - and that's the entire point of data visualization. Why wouldn't I want to pursue it?"
 	},
 	{
 		category:"professional",
@@ -42,18 +42,33 @@ export const faqs=[
 	{
 	category:"personal",
 			question: "Outside of work, what am I passionate about?",
-		answer:"The same passion I have in work - sharing what I love and learn with others - carries on in my hobbies, too. I worked as a rock climbing instructor throughout graduate school and have taught many, many friends how to climb, snowboard, and photograph animals over the years. The high you get from introducing someone you care about to an activity you love - and then they also love it? Amazing."
+		answer:"The same passion I have in work - sharing what I love and learn with others - carries on in my hobbies, too. I worked as a rock climbing instructor throughout graduate school and have taught many, many friends how to climb, snowboard, and photograph animals over the years. The high you get from introducing someone you care about to an activity you love - and then they also love it? Bliss."
 },
 	{
 	category:"professional",
-			question: "What was my first job?",
+			question: "What was my first paying job?",
 		answer:"The official answer is, I was a mathematics tutor at Gordon College (now Gordon State College) while I was dual-enrolling there during high-school. The unofficial answer is, I started working as a soccer referee for Lamar County when I was 13 years old - too young to actually be hired by the government, so I was paid under the table. Shh."
 },
-	// {
-	// 	category:"professional",
-	// 	question: "Have I ever worked outside of academia?",
-	// 	answer:"Yes, but not in a typical 'industry office' situation. All of these were jobs I held part-time while still in school. I interned at Zoo Atlanta in the bird department during my junior year at Georgia Tech (for class credit), was a freelance tutor for a year, and moonlighted at rock climbing gyms in various positions throughout undergrad and graduate school, but most notably as an Advanced Instructor and part-time route setter (memberships are expensive as a student). I've kept myself fairly busy over the years."
-	// },
+	{
+		category:"personal",
+		question: "What's the best book I've read recently?",
+		answer:"'Eve' by Cat Bohannon is a female-biology-focused retelling of the evolution of our species, and I would make every man, woman, and child read it if I could. For girls and women in particular, it's like the User's Guide to our bodies that we were told was around somewhere but could never find when we needed it. Well, here it is! Cat details the evolution of female physiology (e.g. why women have more body fat and what it means for our bodies when we take it out), contextualizes the differences through comparisons to not just male physiology, but also trans and gay biology, and weaves in an analysis of social and cultural practices (holistically, not just American Society^TM) to tie it all together. It is a fantastic, informative, and funny read. 10/10 Recommend."
+	},
+	{
+		category:"personal",
+		question: "Am I an introvert or extrovert?",
+		answer:"I'm your pretty standard introverted nerd, and I have what I call my Emergency Extrovert Button. I hulk-smash it when I need to, but the battery is dependent on the number of people I'm interacting with, the degree of familiarity I have with those people (e.g. friends versus strangers), the level of sustained energy required (e.g. hosting a party vs attending a conference), and of course the duration of the event. The EEB's battery has been known to last a week, but excessive use can lead to both diminishing returns despite recharges and, eventually, to an unavoidable refractory period."
+	},
+	{
+		category:"personal",
+		question: "What is something unique about myself?",
+		answer:"Most of my family lives or has plans to retire to Florida or a desert somewhere. Hot, however, is my least favorite season and I'm the only family member that actively enjoys winter. My philosophy is that if winter only takes from you - your temperature of comfort, your activities, your cute clothes, your ability to even enjoy being outside - then yeah, you're going to hate winter. But winter gives me so much through photography and snowboarding that I can't help but look forward to it. I'm the black, woolly-wrapped sheep of the family."
+	},
+	{
+	category:"professional",
+			question: "What's a mistake I made early on in my career?",
+		answer:"I interned at Zoo Atlanta in the bird department every Saturday for a semester in undergrad. It was an assigned position, I didn't explicitly choose the birds but I wasn't mad about it either. I remember thinking my first day went very well. I enjoyed meeting the other people who worked in birds, as well as the birds of course, and I did everything I was asked to do and did it without any mistakes. But then I got feedback that the department head was concerned I wouldn't be a good fit. I hadn't asked a lot of questions, was very quiet, and I hadn't shown initiative in learning more than what was given to me. Turns out, I had been so nervous and so focused on listening to instructions and following leads that I hadn't talked much period, let alone seemed extroverted and personable. (To be honest, I still feel like the critique about taking initiative to do more might have been more fair after a longer period of time than just a day.) However, it made me re-examine the way I was approaching new working environments. I always try to ask about first day/week/month expectations now so I can be sure that I'm actually off to a good start."
+},
 
 ]
 // {
